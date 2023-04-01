4520ebd97d9f7ba8c6bd1848e4ca501d75f92ec5
Olivier Rogier <github.com@ordisoftware.org>
2023-04-01 12:58:25 +0200

    Update file

--- Project/Source/Forms/MainForm/MainForm.cs
+++ Project/Source/Forms/MainForm/MainForm.cs
@@ -1,7 +1,38 @@
-﻿namespace Ordisoftware.Nutrition.Manager;
+﻿/// <license>
+/// This file is part of Ordisoftware Futhark Decoder.
+/// Copyright 2023-2023 Olivier Rogier.
+/// See www.ordisoftware.com for more information.
+/// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+/// If a copy of the MPL was not distributed with this file, You can obtain one at
+/// https://mozilla.org/MPL/2.0/.
+/// If it is not possible or desirable to put the notice in a particular file,
+/// then You may include the notice in a location(such as a LICENSE file in a
+/// relevant directory) where a recipient would be likely to look for such a notice.
+/// You may add additional accurate notices of copyright ownership.
+/// </license>
+/// <created> 2023-04 </created>
+/// <edited> 2023-04 </edited>
+namespace Ordisoftware.Nutrition.Manager;
 
 public partial class MainForm : Form
 {
+
+  #region Singleton
+
+  /// <summary>
+  /// Indicates the singleton instance.
+  /// </summary>
+  static internal MainForm Instance { get; private set; }
+
+  /// <summary>
+  /// Static constructor.
+  /// </summary>
+  static MainForm()
+  {
+    Instance = new MainForm();
+  }
+
+  #endregion
 
   public MainForm()
   {
--- Project/Source/Program/Program.cs
+++ Project/Source/Program/Program.cs
@@ -1,14 +1,81 @@
-﻿namespace Ordisoftware.Nutrition.Manager;
+﻿/// <license>
+/// This file is part of Ordisoftware Nutrition Manager.
+/// Copyright 2012-2023 Olivier Rogier.
+/// See www.ordisoftware.com for more information.
+/// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+/// If a copy of the MPL was not distributed with this file, You can obtain one at
+/// https://mozilla.org/MPL/2.0/.
+/// If it is not possible or desirable to put the notice in a particular file,
+/// then You may include the notice in a location(such as a LICENSE file in a
+/// relevant directory) where a recipient would be likely to look for such a notice.
+/// You may add additional accurate notices of copyright ownership.
+/// </license>
+/// <created> 2023-04 </created>
+/// <edited> 2023-04 </edited>
+namespace Ordisoftware.Nutrition.Manager;
 
-internal static class Program
+using System.IO.Pipes;
+using System.Runtime.Serialization.Formatters.Binary;
+
+/// <summary>
+/// Provides Program class.
+/// </summary>
+static partial class Program
 {
 
+  /// <summary>
+  /// Indicates the default Settings instance.
+  /// </summary>
+  static public readonly Properties.Settings Settings
+    = Properties.Settings.Default;
+
+  /// <summary>
+  /// Process startup method.
+  /// </summary>
   [STAThread]
   static void Main()
   {
-    Application.EnableVisualStyles();
-    Application.SetCompatibleTextRenderingDefault(false);
-    Application.Run(new MainForm());
+    try
+    {
+      Globals.ChronoStartingApp.Start();
+      Globals.SoftpediaURL = "";
+      Globals.AlternativeToURL = "";
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      /*
+      Language lang = Settings.LanguageSelected;
+      SystemManager.CheckCommandLineArguments<ApplicationCommandLine>(args, ref lang);
+      // No IPCAnswers
+      // No IPCRequests
+      bool upgrade = Settings.UpgradeRequired;
+      Globals.IsSettingsUpgraded = upgrade;
+      Settings.CheckUpgradeRequired(ref upgrade);
+      Settings.UpgradeRequired = upgrade;
+      Globals.IsSettingsUpgraded = Globals.IsSettingsUpgraded && !Settings.FirstLaunch;
+      CheckSettingsReset();
+      if ( lang != Language.None ) Settings.LanguageSelected = lang;
+      SystemManager.TryCatch(Settings.Save);
+      Globals.Settings = Settings;
+      Globals.MainForm = MainForm.Instance;
+      DebugManager.TraceEnabled = Settings.TraceEnabled;
+      DebugManager.Enabled = Settings.DebuggerEnabled;
+      Globals.ChronoStartingApp.Stop();
+      ProcessCommandLineOptions();
+      Globals.ChronoStartingApp.Start();
+      LoadingForm.Instance.Hidden = Settings.LoadingFormHidden;
+      AboutBox.LicenseAsRTF = Properties.Resources.MPL_2_0;
+      AboutBox.DescriptionText = AppTranslations.ApplicationDescription;
+      AboutBox.Instance.ActionViewStats.Enabled = Settings.UsageStatisticsEnabled;
+      */
+      DebugManager.Enabled = true;
+      DebugManager.TraceEnabled = true;
+    }
+    catch ( Exception ex )
+    {
+      ex.Manage();
+    }
+    Application.Run(MainForm.Instance);
+    SystemManager.Exit();
   }
 
 }
